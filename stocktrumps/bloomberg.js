14a6e4fb5e9b888d711353d9b1f1b1d7d50b19ae
Charles Newey <charlie@assemblyco.de>
2015-03-14 20:24:21 +0000

    Add dummy bloomberg data

---
+++ stocktrumps/bloomberg.js
@@ -0,0 +1,45 @@
+var getcards = function () {
+    var data = [
+        {
+            "tick" : "AP",
+            "name" : "apple",
+            "cap" : "1",
+            "price" : 300,
+            "change" : 20
+            },
+        {
+            "tick" : "A",
+            "name" : "a",
+            "cap" : "1",
+            "price" : 300,
+            "change" : 20
+            },
+        {
+            "tick" : "p",
+            "name" : "ple",
+            "cap" : "1",
+            "price" : 300,
+            "change" : 20
+            },
+        {
+            "tick" : "m",
+            "name" : "amm",
+            "cap" : "1",
+            "price" : 300,
+            "change" : 20
+            },
+        {
+            "tick" : "d",
+            "name" : "dd",
+            "cap" : "1",
+            "price" : 300,
+            "change" : 20
+        }
+    ]
+
+    return data;
+};
+
+module.exports = {
+    "getcards": getcards
+};
