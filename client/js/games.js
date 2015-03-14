42fce5376112ee0dbf607a606b3dc3f1c655aec3
Ryan Gouldsmith <RyanGouldsmith@hotmail.co.uk>
2015-03-14 16:47:55 +0000

    Implemented the parsing of the response from the websockets onmessage function call

---
+++ client/js/games.js
@@ -0,0 +1,58 @@
+function Game(){
+
+}
+/**
+* Called when the onmessage function is called in the websockets object
+* Updates the card div every with all the data which is passed in
+* @param JSON object from the server of the card.
+*/
+Game.prototype.showCards = function(){
+    //TODO test data
+    var data = [
+            {
+              "tick" : "APP",
+              "name" : "apple",
+              "cap" : "1",
+              "price" : 300,
+              "change" : 20
+            },
+            {
+                "tick" : "A",
+                "name" : "a",
+                "cap" : "1",
+                "price" : 300,
+                "change" : 20
+            },
+            {
+                "tick" : "p",
+                "name" : "ple",
+                "cap" : "1",
+                "price" : 300,
+                "change" : 20
+            },
+            {
+                "tick" : "mm",
+                "name" : "amm",
+                "cap" : "1",
+                "price" : 300,
+                "change" : 20
+            },
+            {
+                "tick" : "dd",
+                "name" : "dd",
+                "cap" : "1",
+                "price" : 300,
+                "change" : 20
+            }
+    ];
+    //loops through the cards and ouputs the information to the screen
+    //parses the json object passed in.
+    $('.cards-card').each(function(i) {
+        $(this).find(".cards-card-title .name").text(data[i].name);
+        $(this).find(".cards-card-title .tick").text(data[i].tick);
+        $(this).find(".cards-card-data-group #cap").text(data[i].cap);
+        $(this).find(".cards-card-data-group #price").text(data[i].price);
+        $(this).find(".cards-card-data-group #change").text(data[i].change);
+
+    });
+}
