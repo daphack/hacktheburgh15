8a89072ebfa1391ff2ff1f039e82a1607a32eb6f
Ryan Gouldsmith <GouldsmithRyan@hotmail.co.uk>
2015-03-14 17:50:53 +0000

    Added the Ajax call to get the images from google based on the JSON card name

--- client/js/game.js
+++ client/js/game.js
@@ -11,35 +11,35 @@ Game.prototype.showCards = function(){
     var data = [
             {
               "tick" : "AP",
-              "name" : "apple",
+              "name" : "Apple",
               "cap" : "1",
               "price" : 300,
               "change" : 20
             },
             {
                 "tick" : "A",
-                "name" : "a",
+                "name" : "Google",
                 "cap" : "1",
                 "price" : 300,
                 "change" : 20
             },
             {
                 "tick" : "p",
-                "name" : "ple",
+                "name" : "Bloomberg",
                 "cap" : "1",
                 "price" : 300,
                 "change" : 20
             },
             {
                 "tick" : "m",
-                "name" : "amm",
+                "name" : "Twitter",
                 "cap" : "1",
                 "price" : 300,
                 "change" : 20
             },
             {
                 "tick" : "d",
-                "name" : "dd",
+                "name" : "Facebook",
                 "cap" : "1",
                 "price" : 300,
                 "change" : 20
@@ -51,9 +51,35 @@ Game.prototype.showCards = function(){
         $(this).find(".cards-card-title .name").text(data[i].name);
         $(this).find(".cards-card-title .tick").text(data[i].tick);
         $(this).data("tick", data[i].tick);
+        getImage(data[i].name, this);
         $(this).find(".cards-card-data-group #cap").text(data[i].cap);
         $(this).find(".cards-card-data-group #price").text(data[i].price);
         $(this).find(".cards-card-data-group #change").text(data[i].change);
 
+    });
+}
+/**
+* Get the image based on the name from the JSON result
+* Uses jQuery to Change the src attribute to the image.
+* @param name is the name of the card
+* @param cards is a reference to which card we want to update
+*/
+function getImage(name, cards){
+    var url = "";
+    var that = cards;
+    $.ajax({
+        dataType: 'jsonp',
+        url: 'http://ajax.googleapis.com/ajax/services/search/images',
+        data: {
+            v: "1.0",
+            rsz: 1,
+            q: name + " logo"
+        }
+    }).done(function (data){
+        var result = data.responseData.results;
+
+        $.each(result, function (){
+            $(that).find('.cards-card-image').attr('src', this.url);
+        });
     });
 }
