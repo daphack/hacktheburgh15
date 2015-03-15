e0a80a8afb0f65b361c2bc0bf7995304d5c9349d
Lewis Dale <lpd1@aber.ac.uk>
2015-03-15 00:16:13 +0000

    Added distinction between games, and random url generation.
    Now handles new players being added and alerts all other players that a
    a new player has joined

--- stocktrumps/index.js
+++ stocktrumps/index.js
@@ -2,6 +2,8 @@ var WebSocketServer = require('websocket').server;
 var http = require('http');
 var bb = require('./bloomberg.js'); //Bloomberg api
 
+var games = {};
+
 var port = process.env.PORT || 8080;
 var server = http.createServer(function(request, response) {
     response.writeHead(404);
@@ -20,6 +22,7 @@ wsServer.on('request', function(request) {
     var connection = request.accept('echo-protocol', request.origin);
     console.log("Request received");
     connection.send(JSON.stringify({ message: "Message Received" }));
+
     connection.on('message',function(message) {
         if(message.type === 'utf8') {
             try {
@@ -36,12 +39,39 @@ wsServer.on('request', function(request) {
                         connection.send(JSON.stringify(cardObj));
                     } else if(data.function === "startgame") {
 
+                        var url = Math.random().toString(36).replace(/[^a-zA-Z0-9]+/g, '').substr(0, 8);
                         var game = {
                             function : 'startgame',
-                            url : 'lpdrog315'
+                            url : url
                         }
+
+                        games[url] = {};
+                        games[url].connections = [connection];
                         connection.send(JSON.stringify(game));
+
+                    } else if(data.function === "createplayer") {
+                        if(data.game in games) {
+                            games[data.game].connections.push(connection);
+
+                            var len = games[data.game].connections.length;
+
+                            var countObj = {
+                                function: 'createplayer',
+                                count: len
+                            }
+
+                            console.log(len);
+
+                            var connections = games[data.game].connections;
+                            for(var x = 0; x < len; x++) {
+                                var conn = connections[x];
+                                conn.send(JSON.stringify(countObj));
+                            }
+                        }
                     }
+
+                    //TODO: answer, createplayer
+                    //TODO: handle sessions
                 }
             } catch(e) {
                 console.log(e);
