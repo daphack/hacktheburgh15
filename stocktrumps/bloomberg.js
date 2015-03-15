4035c86eddadb5b69f78f0f571c181338cff2dda
Lewis Dale <lpd1@aber.ac.uk>
2015-03-15 02:06:26 +0000

    Modified dummy data

--- stocktrumps/bloomberg.js
+++ stocktrumps/bloomberg.js
@@ -4,35 +4,35 @@ var getcards = function () {
             "tick" : "AP",
             "name" : "apple",
             "cap" : "1",
-            "price" : 300,
+            "price" : 100,
             "change" : 20
             },
         {
             "tick" : "A",
             "name" : "a",
             "cap" : "1",
-            "price" : 300,
+            "price" : 150,
             "change" : 20
             },
         {
             "tick" : "p",
             "name" : "ple",
             "cap" : "1",
-            "price" : 300,
+            "price" : 10,
             "change" : 20
             },
         {
             "tick" : "m",
             "name" : "amm",
             "cap" : "1",
-            "price" : 300,
+            "price" : 800,
             "change" : 20
             },
         {
             "tick" : "d",
             "name" : "dd",
             "cap" : "1",
-            "price" : 300,
+            "price" : 200,
             "change" : 20
         }
     ]
